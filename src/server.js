2498fe3c021728967c9a2d517ea3a7247a3ae1a7
jairogomz <jairogomz@gmail.com>
2026-06-28 03:52:36 -0600

    test(ci): demonstrate lint failure

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import app from './app.js';
 
 const port = process.env.PORT || 3000;
+const unusedVariable = true;
 
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
